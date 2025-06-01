7bc23b7243e74310c13a2ca92af6be55c3e4a9be
Salah-Moftah <132005420+Salah-Moftah@users.noreply.github.com>
2025-06-01 15:17:18 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -73,3 +73,34 @@ document.addEventListener("DOMContentLoaded", () => {
   createListItems(aboutCompanyData, "aboutCompanyList");
   createListItems(contactUsData, "contactUsList");
 });
+
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+let wrapper = document.querySelector(".wrapper"),
+  signupHeader = document.querySelector(".signup h3"),
+  loginHeader = document.querySelector(".login h3");
+
+loginHeader.addEventListener("click", function () {
+  wrapper.classList.add("active");
+});
+signupHeader.addEventListener("click", function () {
+  wrapper.classList.remove("active");
+});
+
